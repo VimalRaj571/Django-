43e79e8f92ad7aa1b85f0eff7cf8780b0eddb232
vimalraj571 <vimalraj.ktm95@gmail.com>
2018-06-21 12:58:37 +0530

    Debug off

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'x0b#$(g-8)ydptq&9wxh6b@+@xnfo30n#4a319i_xy%lfz7(0$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com','[::1]']
 
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog',
+    'blog', 
 ]
 
 MIDDLEWARE = [
